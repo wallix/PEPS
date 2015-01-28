60c930c6de8a641cbf53acbc079353713e036a55
Henri Chataing <chataing.henri@gmail.com>
2015-01-28 17:49:18 +0100

    Increase Haraka message size limit

--- smtpin/haraka.sh
+++ smtpin/haraka.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+# Basic configuration.
+echo "20000000" > /usr/local/haraka/config/databytes
+# SMTP in configuration.
 echo "host = $PEPS_PORT_8999_TCP_ADDR:$PEPS_PORT_8999_TCP_PORT" > /usr/local/haraka/config/smtpin.ini
+# TLS Settings.
 cp /etc/peps/server.key /usr/local/haraka/config/tls_key.pem
 cp /etc/peps/server.crt /usr/local/haraka/config/tls_cert.pem
+# Launch.
 exec haraka -c /usr/local/haraka >>/var/log/haraka.log 2>&1
--- smtpout/haraka.sh
+++ smtpout/haraka.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Basic configuration.
+echo "20000000" > /usr/local/haraka/config/databytes
+# Launch.
 exec haraka -c /usr/local/haraka >>/var/log/haraka.log 2>&1
 
