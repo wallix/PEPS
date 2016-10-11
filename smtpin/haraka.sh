a19e14b03708ed324dd668fe827cddffa6402101
Sebastián Ramírez <tiangolo@gmail.com>
2016-10-11 12:00:44 -0500

    Wait 30 sec to allow PEPS to be ready

--- smtpin/haraka.sh
+++ smtpin/haraka.sh
@@ -3,5 +3,6 @@
 # TLS Settings.
 cp /etc/peps/server.key /usr/local/haraka/config/tls_key.pem
 cp /etc/peps/server.crt /usr/local/haraka/config/tls_cert.pem
-# Launch.
+# Sleep to wait for PEPS to be ready
+sleep 30
 exec haraka -c /usr/local/haraka
